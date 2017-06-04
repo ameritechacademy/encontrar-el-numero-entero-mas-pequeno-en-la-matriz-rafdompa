9abc89b6ee2705d5cb6a111cac652f975990f27d
Rafael Dominguez <rafdompa@gmail.com>
2017-06-04 18:40:40 -0400

    Katas1 Initial

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public static class Kata
+    {
+        public static int FindSmallestInt(int[] args)
+        {
+            if (args.Length == 0)
+                throw new ArgumentException();
+
+            int menor = args[0];
+            foreach (int x in args)
+            {
+                if (x < menor)
+                {
+                    menor = x;
+                }
+            }
+
+            return menor;
+        }
+    }
+}
